68b1f3b1925af6dab201b2e1ca4fc116c49ab161
A1exand9r <dr.dovgalsasha@yandex.ru>
2022-11-12 10:12:00 +0700

    Changes to be committed:

--- Баринов/1самостоятельная.py
+++ Баринов/1самостоятельная.py
@@ -1,6 +1,12 @@
 def inp(text):
-    tp = type(text)
     return text
+
+def styd():
+    sl = dict()
+    for element in range(0,10):
+        
+
 
 if __name__ == '__main__':
     inp(input())
+    styd()
