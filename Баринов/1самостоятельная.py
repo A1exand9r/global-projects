e87c3700443a05d2088d40a4ef987dc1d69fe7ff
A1exand9r <dr.dovgalsasha@yandex.ru>
2022-11-20 20:11:21 +0700

    Changes to be committed:

--- Баринов/1самостоятельная.py
+++ Баринов/1самостоятельная.py
@@ -10,24 +10,41 @@
 
 def text_input():
     text = input()
-
     return text
 
-def slov():
-    student = {'surname': '', 'name': '', 'patronymic': '', 'age': '', 'floor': '', 'group number': '', 'estimation': ''}
 
-    for i in student:
-        print('Введите', i)
-        student[i] = text_input()
-    
-    if int(student['estimation']) <= 5 and int(student['estimation']) >= 3:
-        print('Поехали дальше')
-        print(student)
-        return student
-    else:
-        print('Давай по новой')
-        slov()
+def slovar():
+    slov = {'student1': {'surname': '', 'name': '', 'patronymic': '', 'age': '', 'gender': '', 'number groups': '', 'estimation': ''},
+            'student2': {'surname': '', 'name': '', 'patronymic': '', 'age': '', 'gender': '', 'number groups': '', 'estimation': ''},
+            'student3': {'surname': '', 'name': '', 'patronymic': '', 'age': '', 'gender': '', 'number groups': '', 'estimation': ''},
+            'student4': {'surname': '', 'name': '', 'patronymic': '', 'age': '', 'gender': '', 'number groups': '', 'estimation': ''},
+            'student5': {'surname': '', 'name': '', 'patronymic': '', 'age': '', 'gender': '', 'number groups': '', 'estimation': ''},
+            'student6': {'surname': '', 'name': '', 'patronymic': '', 'age': '', 'gender': '', 'number groups': '', 'estimation': ''},
+            'student7': {'surname': '', 'name': '', 'patronymic': '', 'age': '', 'gender': '', 'number groups': '', 'estimation': ''},
+            'student8': {'surname': '', 'name': '', 'patronymic': '', 'age': '', 'gender': '', 'number groups': '', 'estimation': ''},
+            'student9': {'surname': '', 'name': '', 'patronymic': '', 'age': '', 'gender': '', 'number groups': '', 'estimation': ''},
+            'student9': {'surname': '', 'name': '', 'patronymic': '', 'age': '', 'gender': '', 'number groups': '', 'estimation': ''}}
+
+    for i in slov:
+        print(i)
+        for j in slov[i]:
+            print('Введите', j)
+            slov[i][j] = text_input()
+
+    for i in slov:
+        for j in slov[i]:
+            if slov[i][j] >= '3' and slov[i][j] <= '5':
+                ...
+            else:
+                print('возможные оценки: 5, 4, 3')
+                slov()
+
+        return slov
+            
+
+    print(slov)
+
 
 if __name__ == '__main__':
-    slov()
+    slovar()
 
